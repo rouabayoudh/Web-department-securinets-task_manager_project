36395377c4275ce04c12c4a2f2f1d2500e21afb4
FatSquare <legitcsgo5@gmail.com>
2025-02-01 17:49:06 +0100

    added static projects bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import TopSidebar from "./components/TopSidebar";
+import PorjectSideBar from "./components/PorjectSideBar";
 
 function App() {
-    return <TopSidebar />
+    return (
+        <>
+            <TopSidebar />
+            <PorjectSideBar />
+        </>
+    );
 }
 
 export default App;
